f21f339c72770cb3ca40217f17fd5266102d9408
PARKNAYEON <parkna96@gmail.com>
2021-03-09 17:57:32 +0900

    slackbot model

--- TodoBot/api_v1/todo.py
+++ TodoBot/api_v1/todo.py
@@ -1,25 +1,54 @@
 from flask import jsonify
 from flask import request
 from flask import Blueprint
+from models import Todo, db
 import requests
+import datetime
 
 from . import api
+
+def send_slack(msg):
+    res = requests.post('http://...... slack link.....', json={
+            'text': msg
+    }, headers={'Content-Type' : 'application/json'})
 
 @api.route('/todos', methods=['GET', 'POST'])
 def toods():
     if request.method == 'POST': # 내용을 전달한 데이터를 슬랙에 전달
-        res = requests.post('https://hooks.slack.com/services/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', json={
-            'text': 'Hello world!'
-        }, headers={'Content-Type' : 'application/json'})
+        # 생성하는 코드
+        # 알림
+        send_slack('TODO가 생성되었습니다')# 사용자 정보, 할일 제목, 기한
     elif request.method == 'GET':
         pass
 
     data = request.get_json()
+    return jsonify(data) 
 
-    return jsonify(data)
+@api.route('/slack/todos', methods=['POST'])
+def slack_todos():
+    # flasktodo create aaaaaa : 명령의 구분이 띄어쓰기로 나눠져 있음
+    # flask list 
+    res = request.form['text'].split(' ') # msg 와 cmd 구분하기
+    cmd, *args = res 
 
-@api.route('/test', methods=['POST'])
-def test():
-    res = request.form['text']
-    print(res)
+    ret_msg = ''
+
+    if cmd == 'create':
+        todo_name = args[0]
+
+        todo = Todo()
+        todo.title = todo_name
+
+        db.session.add(todo)
+        db.session.commit()
+        ret_msg = 'todo가 생성되었습니다'
+
+        send_slack('[%s] %s 할일을 만들었습니다.' %(str(datetime.datetime.now()), todo_name)) 
+
+    elif cmd == 'list':
+        todos = Todo.query.all()
+        for todo in enumerate(todos):
+            ret_msg += '%d. %s (~ %s)'%(idx+1, todo.title, str(todo.tstamp))
+        
+
     return jsonify(res)
--- TodoBot/app.py
+++ TodoBot/app.py
@@ -1,8 +1,41 @@
+import os
 from flask import Flask
+from flask import request, redirect, render_template
+from models import db
+from forms import RegisterForm
 from api_v1 import api as api_v1
 
 app = Flask(__name__)
 app.register_blueprint(api_v1, url_prefix='/api/v1') #뷰를 분리할 때 사용
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    return redirect('/')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    return render_template('register.html')
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+dbfile = os.path.join(basedir, 'db.sqlite')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile # 한 줄로 표현 가능 -- connection
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True # tear down일 때 commit을 하겠다 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'adfdffadafagfgad'
+
+db.init_app(app)
+db.app = app
+db.create_all()
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, debug=True)
