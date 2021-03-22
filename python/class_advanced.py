9b27ec33af0b48439ef713f8d98b8fa89699c8dd
PARKNAYEON <parkna96@gmail.com>
2021-03-22 17:36:02 +0900

    django register connect

---
+++ python/class_advanced.py
@@ -0,0 +1,37 @@
+# # 학생1
+# student_name_1 = 'Kim'
+# student_number_1 = 1
+# student_grade_1 = 1
+# student_detail_1 = [
+#     {'gender' : 'Male'},
+#     {'score1' : 95},
+#     {'score2' : 88}
+# ]
+
+# # 학생2
+# student_name_2 = 'Lee'
+# student_number_2 = 2
+# student_grade_2 = 2
+# student_detail_2 = [
+#     {'gender' : 'Female'},
+#     {'score1' : 77},
+#     {'score2' : 92}
+# ]
+
+# # 학생3
+# student_name_3 = 'Park'
+# student_number_3 = 3
+# student_grade_3 = 4
+# student_detail_2 = [
+#     {'gender' : 'Male'},
+#     {'score1' : 99},
+#     {'score2' : 100}
+# ]
+
+# # 리스트 구조
+# student_names_list = ['Kim', 'Lee', 'Park']
+# student_numbers_list = [1, 2, 3]
+# student_grades_list = [1, 2, 4]
+# student_details_list = [
+
+# ]
